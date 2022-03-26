0bcadd4983ab1a2620f5f36c87e2f3e2f651c45a
Sébastien Guérin <sebast.guerin@gmail.com>
2022-03-26 17:29:14 +0100

    refactor

--- fundamentals/property-based-test/roman.go
+++ fundamentals/property-based-test/roman.go
@@ -2,12 +2,34 @@ package main
 
 import "strings"
 
-type RomanNumeral struct {
+type romanNumeral struct {
 	Value  int
 	Symbol string
 }
 
-var allRomanNumerals = []RomanNumeral{
+type romanNumerals []romanNumeral
+
+func (r romanNumerals) ValueOf(symbols ...byte) int {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return s.Value
+		}
+	}
+	return 0
+}
+
+func (r romanNumerals) Exists(symbols ...byte) bool {
+	symbol := string(symbols)
+	for _, s := range r {
+		if s.Symbol == symbol {
+			return true
+		}
+	}
+	return false
+}
+
+var allRomanNumerals = romanNumerals{
 	{1000, "M"},
 	{900, "CM"},
 	{500, "D"},
@@ -34,4 +56,32 @@ func ConvertToRoman(n int) string {
 	}
 
 	return result.String()
+}
+
+func ConvertToArabic(roman string) int {
+	total := 0
+	for i := 0; i < len(roman); i++ {
+		symbol := roman[i]
+
+		if couldBeSubstractive(i, symbol, roman) {
+			if val := allRomanNumerals.ValueOf(symbol, roman[i+1]); val != 0 {
+				total += val
+				i++ // move past next character
+			} else {
+				total += allRomanNumerals.ValueOf(symbol)
+			}
+		} else {
+			total += allRomanNumerals.ValueOf(symbol)
+		}
+	}
+
+	return total
+}
+
+func isSubstractiveSymbol(symbol uint8) bool {
+	return symbol == 'I' || symbol == 'X' || symbol == 'C'
+}
+
+func couldBeSubstractive(index int, currentSymbol uint8, roman string) bool {
+	return index+1 < len(roman) && isSubstractiveSymbol(currentSymbol)
 }
